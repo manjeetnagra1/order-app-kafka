c6cae42a5f55115724b3d03ed44f5fdfb567dabb
Manjeet Nagra <manjeet.nagra@ditstek.com>
2025-04-16 15:08:59 +0530

    Fixed issues in Config module

--- config/src/main/java/com/kafka/config/config/config/KafkaConfiguration.java
+++ config/src/main/java/com/kafka/config/config/config/KafkaConfiguration.java
@@ -1,4 +1,4 @@
-package com.kafka.config;
+package com.kafka.config.config.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.TopicPartition;
---
+++ config/src/main/java/com/kafka/config/config/config/KafkaConfigurationApplication.java
@@ -0,0 +1,11 @@
+package com.kafka.config.config.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConfigurationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConfigurationApplication.class, args);
+    }
+}
